3bdf7e0c9bb56ad592e3c5093a5929f4437e2a48
Brandon Valosek <bvalosek@gmail.com>
2012-02-22 15:30:04 -0600

    First commit

---
+++ src/com/bvalosek/psychtypes/TestApp.java
@@ -0,0 +1,21 @@
+//-----------------------------------------------------------------------------
+//
+// (C) Brandon Valosek, 2012 <bvalosek@gmail.com>
+//
+//-----------------------------------------------------------------------------
+
+package com.bvalosek.psychtypes;
+
+/**
+ *
+ * Application for testing out the psychtypes stuff
+ *
+ */
+public class TestApp {
+
+    /** app entry point */
+    public static void main(String[] args) {
+        System.out.println( "Hello World!" );
+    }
+
+}
